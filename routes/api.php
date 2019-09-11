3f0f79d2fd953b0bfdda056e16766055d323ec73
faustino_vasquez <faustino.vasquez@mitechnologiesinc.com>
2019-09-10 17:00:54 -0700

    topics and posts

--- routes/api.php
+++ routes/api.php
@@ -4,3 +4,10 @@ Route::post('/register', 'AuthController@register');
 Route::post('/login', 'AuthController@login');
 Route::get('/user', 'AuthController@user');
 Route::post('/logout', 'AuthController@logout');
+
+Route::prefix('topics')
+    ->middleware('auth:api')
+    ->group(function(){
+    Route::post('/', 'TopicController@store');
+});
+
